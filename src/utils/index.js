ee7ddf73d6fa22c0ae7ec00333819f8edfa15fb2
sachind3 <desai.sachin45@gmail.com>
2024-05-23 20:21:29 +0530

    wip

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getAPI = (token) => {
   const API = axios.create({
     baseURL:
-      process.env.REACT_APP_ENVIRONMENT === "DEVELOPMENT"
+      process.env.REACT_APP_ENVIRONMENT === "PRODUCTION"
         ? process.env.REACT_APP_DEV_API_URL
         : process.env.REACT_APP_PROD_API_URL,
     withCredentials: true,
