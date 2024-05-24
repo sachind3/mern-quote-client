118a458913e34ad0bd483b9bbf5e390d1f26b147
sachind3 <desai.sachin45@gmail.com>
2024-05-25 00:08:31 +0530

    added menu dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,30 +3,75 @@ import { MdLogout } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { actionLogout } from "../redux/features/userSlice";
+import { useState, useEffect, useRef } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Header = () => {
   const dispatch = useDispatch();
+  const [showDropdown, setShowDropdown] = useState(false);
   const { user } = useSelector((state) => state.user);
+
+  const dropdownRef = useRef();
+
   const handleLogout = () => {
     dispatch(actionLogout());
   };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setShowDropdown(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <header className="bg-white/50 backdrop-blur sticky top-0 shadow-sm py-2 z-50 border-b border-b-slate-200">
-      <div className="max-w-6xl px-4 mx-auto flex items-center justify-between">
+    <header className="bg-white/50 backdrop-blur sticky top-0 shadow-sm z-50 border-b border-b-slate-200 ">
+      <div className="max-w-6xl px-4 mx-auto flex items-center justify-between h-12">
         <Link to="/" className="text-orange-500 font-extrabold text-2xl">
           <span className="text-amber-500">Quote</span>Hub
         </Link>
         <div className="flex items-center gap-3">
           {user ? (
             <>
-              <Link to="/quote/create">Add a quote</Link>
-              <Link to="/quote/myquotes">My quotes</Link>
-              <Link to="/user/profile">
-                <FaRegUser />
-              </Link>
-              <button onClick={handleLogout}>
-                <MdLogout />
-              </button>
+              <div
+                className="relative cursor-pointer"
+                onClick={() => setShowDropdown(!showDropdown)}
+                ref={dropdownRef}
+              >
+                <FaRegUser className="text-xl" />
+                <AnimatePresence>
+                  {showDropdown && (
+                    <motion.div
+                      className="card !bg-white absolute w-44 text-sm flex flex-col gap-3 right-0 top-8"
+                      initial={{ opacity: 0, y: -10 }}
+                      animate={{ opacity: 1, y: 0 }}
+                      exit={{ opacity: 0, y: -10 }}
+                      transition={{ duration: 0.2 }}
+                    >
+                      <Link to="/user/profile">
+                        <div className="line-clamp-1">Sachin Desai</div>
+                        <div className="line-clamp-1 text-xs">
+                          desai.sachin45@gmail.com
+                        </div>
+                      </Link>
+                      <Link to="/quote/myquotes">My Quotes</Link>
+                      <Link to="/quote/create">Add a quote</Link>
+                      <button
+                        onClick={handleLogout}
+                        className="flex gap-2 items-center text-red-600"
+                      >
+                        <MdLogout /> Logout
+                      </button>
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </div>
             </>
           ) : (
             <Link to="/user/login">Login</Link>
